f772612deb6d177ecd7a5a786f3843c4c84b6c63
Maksim Makridin <maginot.fireline@gmail.com>
2020-12-20 19:17:29 +0300

    Renewed hash function for strings

--- hashes/include/hashes.hpp
+++ hashes/include/hashes.hpp
@@ -8,14 +8,38 @@
 #ifndef HASHES_HPP
 #define HASHES_HPP
 
-const uint64_t BigPrime = 2305843009213693951;
-// const uint64_t BigPrime = 2305843009213693967;
-// const uint64_t BigPrime = 9007199254740991;
-// const uint64_t BigPrime = 9007199254740881;
-// const uint64_t BigPrime = 2147483647;
-// const uint64_t BigPrime = 23456789;
-// const uint64_t BigPrime = 2059039;
-// const uint64_t BigPrime = 2001511;
+//const uint64_t BigPrime = 2305843009213693951;
+//const uint64_t BigPrime = 2305843009213693967;
+//const uint64_t BigPrime = 9007199254740991;
+//const uint64_t BigPrime = 9007199254740881;
+//const uint64_t BigPrime = 67280421310721;  // (2^64+1)/274177
+//const uint64_t BigPrime = 2147483647;  // 8th Mersenne number, 2^31-1
+//const uint64_t BigPrime = 23456789;
+//const uint64_t BigPrime = 2059039;
+//const uint64_t BigPrime = 2001511;
+//const uint64_t BigPrime = 331999;
+//const uint64_t BigPrime = 27361;
+//const uint64_t BigPrime = 5039;
+const std::vector<uint64_t> primes_asc{
+    167,
+    239,
+    317,
+    401,
+    719,
+    1193,
+    1931,
+    3119,
+    5039,
+    7793,
+    9377,
+    11939,
+    18253,
+    22189,
+    27361,
+    331999,
+    2001511
+};
+
 
 std::mt19937 gen{};
 
@@ -52,8 +76,8 @@ We are casting everything to static_cast<size_t>
 /*
     Hash rule:
     m is pwr(2, M), table size
-    w = 64
-    a is from {0, .. pwr(2, w} - 1}, odd number
+    w = 64 - machine word length for best performance
+    a is odd from {0, .. pwr(2, w} - 1}
     b is from {0, .. pwr(2, w - M} - 1}
     h(a, b) = (uint64_t)(ax + b) >> (w - M)
  */
@@ -96,67 +120,49 @@ void CustomHash<Key>::set_hash_parameters()
     b_param = dist_b(gen);
 };
 
+template <typename Hash>
 class CustomHashStrings {
 protected:
     uint64_t m{2};
     uint64_t M{1};
     uint64_t w{64};
+    uint64_t p{0};
     uint64_t a_param{0};
     size_t curr_hash{0};
+    Hash hash{};
 public:
     CustomHashStrings(){};
     void set_table_size(const uint64_t m, const uint64_t M)
     {
         this->m = m;
         this->M = M;
+        for (auto elem : primes_asc) {
+            if (elem >= m) {
+                p = elem;
+                break;
+            }
+        }
+        hash.set_table_size(m, M);
     };
     void set_hash_parameters();
     size_t operator()(const std::string& key) {
         size_t length = key.size();
-        this->curr_hash = 0;
+        curr_hash = 0;
         for (size_t i = 0; i < length; ++i) {
             curr_hash += static_cast<size_t>(
-                (static_cast<uint64_t>(key[i] * std::pow(a_param, i))) % BigPrime
+                static_cast<uint64_t>(static_cast<uint64_t>(key[i]) * std::pow(a_param, i)) % p % m
             );
         }
+        curr_hash = hash(curr_hash);
         return curr_hash;
     };
 
 };
 
-void CustomHashStrings::set_hash_parameters() {
-    std::uniform_int_distribution<uint64_t> dist_a{0, static_cast<uint64_t>(uint64_t(1) << w) - 1};
+template <typename Hash>
+void CustomHashStrings<Hash>::set_hash_parameters() {
+    std::uniform_int_distribution<uint64_t> dist_a{0, p};
     a_param = dist_a(gen);
-    //to get odd number
-    if (!(a_param & 1))
-        a_param++;
 };
-
-// class CustomHash_Strings {
-// private:
-//     uint64_t a_param{0};
-//     // with this default table size, this function can be used w/o any hash at all
-//     size_t table_size{1};
-// public:
-//     CustomHash_Strings();
-//     explicit CustomHash_Strings(size_t table_size);
-
-//     void resize(size_t n);
-//     size_t operator()(const std::string& key);
-// };
-
-// CustomHash_Strings::CustomHash_Strings(size_t table_size): table_size{table_size} {
-//     a_param = dist(gen);
-// };
-
-// void CustomHash_Strings::resize(size_t n) {
-//     this->table_size = n;
-// };
-
-// size_t CustomHash_Strings::operator()(const std::string& key) {
-//     return static_cast<size_t>(
-//         (static_cast<uint64_t>(key) * a_param) % BigPrime % table_size
-//     );
-// };
 
 #endif //HASHES_HPP
